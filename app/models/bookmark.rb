0847fafb3fffe150ff0a9a3ada2ffaa6de5509fb
carson mulligan <carsonmulligan45@gmail.com>
2022-05-19 12:57:32 +0100

    FEAT: 19/19

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
   validates_uniqueness_of :movie, scope: :list
-  ## validates that pairings are unique
   validates :comment, length: { minimum: 6 }
 end
